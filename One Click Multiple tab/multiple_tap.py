f26224b1769534359b7d00d21392fa76f55f6aae
Manish kasera <66238224+Manish-kasera@users.noreply.github.com>
2021-06-13 21:24:11 +0530

    Add files via upload

---
+++ One Click Multiple tab/multiple_tap.py
@@ -0,0 +1,14 @@
+import webbrowser
+import pywhatkit
+
+webbrowser.open("https://mail.google.com/mail/u/0/#inbox")
+webbrowser.open("https://github.com/")
+webbrowser.open("https://discord.com/channels/811724426965811210/811724426965811213")
+webbrowser.open("https://docs.python.org/3.9/tutorial/classes.html")
+
+pywhatkit.playonyt("pran")
+
+webbrowser.open_new("https://gssoc.girlscript.tech/")
+
+
+
